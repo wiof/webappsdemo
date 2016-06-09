501b39f50d874154676708825d3f1186eeb12a66
Javier Moreno <Javier Moreno>
2016-06-09 10:52:39 +0200

    First version.

---
+++ CibDemo/Controllers/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CibDemo.Controllers
+{
+    public class HealthController : ApiController
+    {
+        private static bool Healthy { get; set; } = true; 
+
+        // GET: api/Health
+        public Object Get()
+        {
+            if (Healthy == false)
+            {
+                throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+
+            return new { healthy = Healthy };
+        }
+
+
+        // POST: api/Health
+        public Object Post([FromBody]string value)
+        {
+            Healthy = Convert.ToBoolean(value);
+            return new { healthy = Healthy };
+        }
+
+    }
+}
---
+++ CibDemo/Controllers/PiController.cs
@@ -0,0 +1,63 @@
+﻿using CibDemo.Models;
+using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CibDemo.Controllers
+{
+    public class PiController : ApiController
+    {
+        private static int Invocations { get; set; }
+        
+
+        // GET api/values/5
+        public Object Get(int id)
+        {
+            Invocations++;
+            Trace.TraceInformation("Otro cliente al que atender.");
+            if (id < 10)
+            {   var ex = new ArgumentException("Requerida una precisión superior a 10.");
+
+                // Telemetría para App Insights
+                var telemetry = new TelemetryClient();
+                var telemetryInfo = new Dictionary<string, string>
+                { { "invocaciones", Convert.ToString(Invocations) } };
+                var telemetryMeasurements = new Dictionary<string, double>
+                { { "digitos", id } };
+                telemetry.TrackException(ex, telemetryInfo, telemetryMeasurements);
+
+                // Mensaje para Applications Logs
+                Trace.TraceError("El cliente anda despistado.");
+
+                throw ex;
+            }
+            BigNumber x = new BigNumber(id);
+            BigNumber y = new BigNumber(id);
+            x.ArcTan(16, 5);
+            y.ArcTan(4, 239);
+            x.Subtract(y);
+            
+            string pi = x.AsPrintableString();
+
+            if (pi.Length > 1000)
+            {
+                Trace.TraceWarning("Resultado de " + pi.Length + " caracteres!");
+            }
+
+            var resultado = new
+            {
+                pi = pi,
+                invocations = Invocations
+            };
+
+            return resultado;
+        }
+
+ 
+    }
+}
